cbfed72db3cd28bb9db5107da8eca89497fe1599
Mudra Verma <47663461+mudraverma65@users.noreply.github.com>
2026-06-18 21:28:11 -0400

    Add: number-of-provinces - submission-1

---
+++ Data Structures & Algorithms/number-of-provinces/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int provinces = 0;
+        int len = isConnected.length;
+        int[] visited = new int[len];
+
+        Queue<Integer> line = new LinkedList<>();
+
+        for(int i = 0; i<len; i++){
+            visited[i] = -1;
+        }
+
+        for(int i = 0; i<len; i++){
+            if(visited[i] != -1){
+                continue;
+            }
+            visited[i] = 1;
+            line.offer(i);
+            while(!line.isEmpty()){
+                int curr = line.poll();
+                for(int j=0; j<len; j++){
+                    if(visited[j] == -1 && j != curr && isConnected[curr][j] == 1){
+                        visited[j] = 1;
+                        line.offer(j);
+                    }
+                }
+            }
+            provinces++;
+        }
+
+        return provinces;
+    }
+
+}
